57eae5357ba89fee5ee2fc667978a3d4c498c066
Brandon <brandon@weonly.win>
2026-05-22 18:13:46 -0500

    feat: initialize Go module with dependencies

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/BurntSushi/toml"
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	// dexmon - Dexcom Share CGM daemon with Pushover alarms
+	// Placeholder entry point - implementation in Task 13
+}
